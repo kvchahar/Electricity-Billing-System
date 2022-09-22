3f044b1cde874404a1e97dda37fe7e22b3646af6
user <krishnaveer.chahar_cs19@gla.ac.in>
2022-09-22 13:05:18 +0530

    Created Connection to database

--- src/electricity/database/Conn.java
+++ src/electricity/database/Conn.java
@@ -1,4 +1,13 @@
 package electricity.database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Conn {
+    public Conn() throws SQLException {
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/ebs", "root", "");
+        Statement statement = con.createStatement();
+    }
 }
