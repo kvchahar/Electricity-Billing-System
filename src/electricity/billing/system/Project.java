3cddfadfdc5ab0d072752c5df7cb4143a63a349a
user <krishnaveer.chahar_cs19@gla.ac.in>
2022-09-22 13:52:54 +0530

    Added Project.java

---
+++ src/electricity/billing/system/Project.java
@@ -0,0 +1,13 @@
+package electricity.billing.system;
+
+import javax.swing.*;
+
+public class Project extends JFrame {
+    public Project() {
+
+    }
+
+    public static void main(String[] args) {
+        new Project();
+    }
+}
